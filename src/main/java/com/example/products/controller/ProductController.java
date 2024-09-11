7e5b3bd94316cc8f5a09a31d25e169560905126b
Arina <arina7077077@mail.ru>
2024-09-11 20:07:55 +0600

    added logging

--- src/main/java/com/example/products/controller/ProductController.java
+++ src/main/java/com/example/products/controller/ProductController.java
@@ -4,6 +4,9 @@ import com.example.products.model.Product;
 import com.example.products.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -12,16 +15,20 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     private ProductRepository productRepository;
 
     @GetMapping
     public List<Product> getAllProducts() {
+        logger.info("Был выполнен запрос на получение списка продуктов");
         return productRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id) {
+        logger.info("Был выполнен запрос на получение продукта с id " + id);
         Optional<Product> product = productRepository.findById(id);
         return product.orElse(null);
     }
@@ -33,6 +40,8 @@ public class ProductController {
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        logger.info("Продукт с id " + id + " был обновлен");
+
         Product product = productRepository.findById(id).orElseThrow();
         product.setName(productDetails.getName());
         product.setDescription(productDetails.getDescription());
